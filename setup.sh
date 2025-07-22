92b822d4b04971cb687097a106538f8d1d5bc609
basharalmansour <126420869+basharalmansour@users.noreply.github.com>
2025-07-22 22:15:53 +0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pip install -U pip setuptools wheel
-pip install -r requirements.txt
-python -m spacy download en_core_web_sm
-python -m nltk.downloader punkt
-python -m nltk.downloader stopwords
